0d53f05b0fc2d3bf5224af009664f355ca85b808
tuphan-dn <tuphan@descartes.network>
2022-06-23 18:09:25 +0700

    remove native embeded view

--- packages/senhub/src/index.tsx
+++ packages/senhub/src/index.tsx
@@ -42,9 +42,6 @@ export * from 'shared/runtime'
 // Session/Storage
 export { default as session } from 'shared/session'
 export { default as storage } from 'shared/storage'
-// Embeded View
-export { default as EmbededView } from 'shared/embededView'
-export * from 'shared/embededView'
 // Token Provider
 export { default as TokenProvider } from 'shared/tokenProvider'
 // Utility
