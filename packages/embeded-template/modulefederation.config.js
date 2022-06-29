5002f9f52f557acf84ca4cbd762c0dcf71c2b5ba
tuphan-dn <tuphan@descartes.network>
2022-06-29 14:26:05 +0700

    remove required version of @sentre/senhub

--- packages/embeded-template/modulefederation.config.js
+++ packages/embeded-template/modulefederation.config.js
@@ -1,5 +1,4 @@
 const name = process.env.REACT_APP_ID
-const { version: senhubVersion } = require('./package.json')
 
 module.exports = {
   name,
@@ -11,7 +10,7 @@ module.exports = {
     '@reduxjs/toolkit': { singleton: true, requiredVersion: '^1.6.2' },
     'react-redux': { singleton: true, requiredVersion: '^7.2.5' },
     antd: { singleton: true, requiredVersion: '^4.21.0' },
-    '@sentre/senhub': { singleton: true, requiredVersion: senhubVersion },
+    '@sentre/senhub': { singleton: true },
   },
   exposes: {
     './bootstrap': 'bootstrap.app',
